c68973627744e81034e763681edbfb3690f37881
AnkitaKittur <ankita07kittur@gmail.com>
2017-05-10 12:12:48 -0700

    HII

--- src/test1/LightBulbTest.java
+++ src/test1/LightBulbTest.java
@@ -20,6 +20,7 @@ public class LightBulbTest {
 		//fail("Not yet implemented");
 		assertEquals(2000,LightBulbTest1.getEnergy());
 		System.out.println("GOOD MORNING");
+		System.out.println("HAVE A NICE DAY");
 	}
 
 }
